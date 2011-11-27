83a848852e645afac84e4b34ea54256944e92daa
Aad Versteden <madnificent@gmail.com>
2011-11-27 22:08:37 +0100

    updating tests for the new internal representation of tests

--- is-right-packagetests.lisp
+++ is-right-packagetests.lisp
@@ -2,29 +2,29 @@
 (rm-tests (common-lisp:find-package ':is-right))
 
 (test 'is-right::make-test-for-function-execution
-      (is-right::same '(test '+ (is-right::same 3 (+ 1 2)))
+      (is-right::same '(test '+ '(is-right::same 3 (+ 1 2)))
                       (is-right::make-test-for-function-execution '+ '(+ 1 2))))
 
 (test 'is-right::make-test-for-function-execution
       (is-right::same
        '(test 'loop
-              (is-right::same 1
-                              (loop for x from 0 below 2
-                                    sum x)))
+              '(is-right::same 1
+                               (loop for x from 0 below 2
+                                     sum x)))
        (is-right::make-test-for-function-execution 'loop
                                                    '(loop for x from 0 below 2
                                                           sum x))))
 
 (test 'is-right::make-get-right-test
-      (is-right::same '(test '+ (is-right::same 6 (+ 1 2 3)))
+      (is-right::same '(test '+ '(is-right::same 6 (+ 1 2 3)))
                       (is-right::make-get-right-test '(+ 1 2 3))))
 
 (test 'is-right::make-get-right-test
       (is-right::same
        '(test '+
-              (is-right::same 6
-                              (let ((x 1))
-                                (+ x 2 3))))
+              '(is-right::same 6
+                               (let ((x 1))
+                                 (+ x 2 3))))
        (is-right::make-get-right-test '+
                                       '(let ((x 1))
                                          (+ x 2 3)))))
@@ -84,29 +84,29 @@
                (list 'hash-table
                      (list 'make-hash-table ':test (list 'quote 'equal))))
               (list 'setf (list 'gethash "foo" 'hash-table) "bar")
-              (list 'setf (list 'gethash 'epicdb-tests::foo 'hash-table)
+              (list 'setf (list 'gethash 'is-right-tests::foo 'hash-table)
                     "bang")
               'hash-table)
         (make-print-form
-         (let ((epicdb-tests::table (make-hash-table :test 'equal)))
-           (setf (gethash "foo" epicdb-tests::table) "bar")
-           (setf (gethash 'epicdb-tests::foo epicdb-tests::table) "bang")
-           epicdb-tests::table))))
+         (let ((is-right-tests::table (make-hash-table :test 'equal)))
+           (setf (gethash "foo" is-right-tests::table) "bar")
+           (setf (gethash 'is-right-tests::foo is-right-tests::table) "bang")
+           is-right-tests::table))))
 
 (test 'print-form
-      '(let ((epicdb-tests::table (make-hash-table :test 'equal)))
-         (setf (gethash "foo" epicdb-tests::table) "bar")
-         (setf (gethash 'epicdb-tests::foo epicdb-tests::table) "bang")
-         (setf (gethash 'epicdb-tests::baz epicdb-tests::table) nil)
+      '(let ((is-right-tests::table (make-hash-table :test 'equal)))
+         (setf (gethash "foo" is-right-tests::table) "bar")
+         (setf (gethash 'is-right-tests::foo is-right-tests::table) "bang")
+         (setf (gethash 'is-right-tests::baz is-right-tests::table) nil)
          (same (list "bar" 't)
-               (multiple-value-list (gethash "foo" epicdb-tests::table)))
+               (multiple-value-list (gethash "foo" is-right-tests::table)))
          (same (list "bang" 't)
                (multiple-value-list
-                (gethash 'epicdb-tests::foo epicdb-tests::table)))
+                (gethash 'is-right-tests::foo is-right-tests::table)))
          (same (list 'nil 't)
                (multiple-value-list
-                (gethash 'epicdb-tests::baz epicdb-tests::table)))
+                (gethash 'is-right-tests::baz is-right-tests::table)))
          (same (list 'nil 'nil)
                (multiple-value-list
-                (gethash 'epicdb-tests::bii epicdb-tests::table)))))
+                (gethash 'is-right-tests::bii is-right-tests::table)))))
 
